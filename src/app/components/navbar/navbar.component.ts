2df38de906db79d450b94d386fedff3aecb25b16
IDGS-903-19002425 <19002425@alumnosutleon.edu.mx>
2025-08-03 21:58:41 -0600

    Avance minuto 2:11:57

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -6,18 +6,29 @@ import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 import {MatMenuModule} from "@angular/material/menu"
 import { CommonModule } from '@angular/common';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-navbar',
-  imports: [MatToolbarModule,MatIconModule,MatMenuModule,MatButtonModule,RouterLink,CommonModule],
+  imports: [MatToolbarModule,MatIconModule,MatMenuModule,MatButtonModule,RouterLink,CommonModule,MatSnackBarModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
 authService = inject(AuthService);
 router = inject(Router);
+matSnackBar = inject(MatSnackBar);
 
 isLoggedIn(){
   return this.authService.isLoggedIn();
 }
+
+logout=()=>{
+  this.authService.logout();
+  this.matSnackBar.open('Sesion cerrada', 'Close',{
+    duration: 5000,
+    horizontalPosition: "center",
+  });
+  this.router.navigate(["/login"]);
+};
 }
